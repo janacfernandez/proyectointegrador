4c874100519498657a03304ea792db90c04e507b
SantinoTabossi <stabossiturner@udesa.edu.ar>
2021-06-06 19:17:31 -0300

    detail album

--- js/detail-album.js
+++ js/detail-album.js
@@ -33,23 +33,90 @@ window.addEventListener("load", function () {
         let idAlbum = queryStringObj.get('id');
         let cambioJava = document.querySelector("main")
     
-        console.log(album);
+        console.log(idAlbum)
+
+       
         
-        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/albums/${idAlbum}`)
+        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/${idAlbum}`)
         .then(function (response) {
             return response.json();
 
         })
         .then(function (datos) {
-            
-       
+            let basico=datos
+            console.log(basico);
 
-            .catch(function (error) {
-                console.log(error);
-            })
+            let titulo=datos.title;
+            console.log(titulo);
+
+            let artista=datos.artist.name
+            console.log(artista);
+
+            let foto=datos.cover
+            console.log(foto)
+
+
+let tracklist=basico.tracks.data.title
+console.log(tracklist)
+
+
+
+ 
+cambioJava.innerHTML += ` <section class="main-alb">
+<article class="alb-info">
+      <h2>${titulo}</h2>
+      <img src="${foto}" alt="Iluminate">
+      <a href="/detail-artist.html">
+          <h3>${artista}</h3>
+      </a>
+      <p>Genero: <a href="detail-genres.html">Pop</a></p>
+  </article>
+  <article class="tracklist">
+      <h3>Tracklist</h3>
+      <ol>
+          <li>Ruin</li>
+          <li>Mercy</li>
+          <li>Treat You Better</li>
+          <li>Three Empty word</li>
+          <li>Don't Be A Fool</li>
+          <li>Like This</li>
+          <li>No promises</li>
+          <li>Lights on</li>
+          <li>Honest</li>
+          <li>Patience</li>
+          <li>Bad Reputation</li>
+          <li>Understand</li>
+          <li>There's Nothing Holdin' Me Back</li>
+      </ol>
+  </article>
+</section>
+`;
+
+
+
         })
+            
 
+        .catch(function (error) {
+                console.log(error);
         
+            })
+        
+
+            fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${idAlbum}`)
+            .then(function (response) {
+                return response.json();
+    
+            })
+            .then(function (datos) {
+                console.log(datos)
+                
+            })
+                
+    
+            .catch(function (error) {
+                    console.log(error);
+                })          
 
 
 //onload    
